ba04909a9847c45b66aa23dce7cade8aefbe4d99
Chris Sakai <cs.chris.sakai@gmail.com>
2016-09-27 17:36:34 -0400

    Add auto injecting of src files.

--- server/index.js
+++ server/index.js
@@ -3,10 +3,18 @@
 
 var express = require('express'),
     bodyParser = require('body-parser'),
+    glob = require('glob'),
     path = require('path'),
     app = express(),
     port = process.env.PORT || 8000,
-    routes;
+    src_deps = glob.sync('**/*.module.js', {
+        cwd: 'client',
+        ignore: '**/*.spec.js'
+    }).concat(glob.sync('**/*.js', {
+        cwd: 'client',
+        ignore: '**/*.spec.js'
+    })),
+    vendor_deps = require('./client-deps');
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
@@ -16,13 +24,16 @@ app.get('/ping', function(req, res, next) {
     console.log(req.query);
     res.send('pong');
 });
-app.use("/src", express.static(path.resolve(__dirname + "/../client/app/")));
+app.use("/src", express.static(path.resolve(__dirname + "/../client/")));
 app.use("/assets", express.static(path.resolve(__dirname + "/../client/assets")));
 app.use("/vendor", express.static(path.resolve(__dirname + "/../node_modules/")));
 
 app.use(require('./util/error_handler'));
 app.get("/*", function (req, res) {
-    res.render('index', { deps: require('./client-deps') });
+    res.render('index', {
+        vendor_deps: vendor_deps,
+        src_deps: src_deps
+    });
 });
 app.listen(port, function() {
     console.log('************************');
