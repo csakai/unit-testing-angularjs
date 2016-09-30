976c1e668b0c5943759d40b2c70e989515fb9e3b
Chris Sakai <cs.chris.sakai@gmail.com>
2016-09-30 17:13:13 -0400

    Remove debug stuff

--- client/app.module.js
+++ client/app.module.js
@@ -2,13 +2,4 @@
     var app = angular.module('app', [
         'app.items'
     ]);
-
-    app.run(function(Items) {
-        Items.index().then(function(data) {
-            console.log('data', data);
-        });
-//        Items.add('nurrr').then(function(data) {
-//            console.log('data?', data);
-//        });
-    });
 })();
